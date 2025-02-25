856ef939be3e1299a4a13cd18cedf1fac1f5014b
North-Wang <103781180+North-Wang@users.noreply.github.com>
2025-02-25 09:54:12 +0800

    save

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias:{
-     '@': resolve(__dirname, 'src'),
-    }
-  },
+  
   vite:{
     css: {
       preprocessorOptions: {
